548111a89a398d7c60bbd83bfc6a84e5b48fbf38
jsc284 <jsc284@scarletmail.rutgers.edu>
2021-10-21 00:42:16 -0400

    Edit 10: Making the Chain Scoring Work
    Created script "ScoreManager". Attached script "ScoreManager" to object "_MainCamera". Edited script "ScoreManager" according to pages 686-688.
    Edited script "Prospector" according to pages 688-689.

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -198,6 +198,7 @@ public class Prospector : MonoBehaviour {
 				MoveToDiscard(target);
 				MoveToTarget(Draw());
 				UpdateDrawPile();
+				ScoreManager.EVENT(eScoreEvent.draw);
 				break;
 
 			case eCardState.tableau:
@@ -215,6 +216,7 @@ public class Prospector : MonoBehaviour {
 				tableau.Remove(cd);
 				MoveToTarget(cd);
 				SetTableauFaces();
+				ScoreManager.EVENT(eScoreEvent.mine);
 				break;
         }
 		CheckForGameOver();
@@ -248,10 +250,12 @@ public class Prospector : MonoBehaviour {
     {
 		if (won)
         {
-			print("Game Over. You won! :)");
+			// print("Game Over. You won! :)");
+			ScoreManager.EVENT(eScoreEvent.gameWin);
         } else
         {
-			print("Game Over. You lost. :(");
+			// print("Game Over. You lost. :(");
+			ScoreManager.EVENT(eScoreEvent.gameLoss);
         }
 		SceneManager.LoadScene("__Prospector_Scene_0");
     }
