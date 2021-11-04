6e814ba247358a1fb51ade7ef01219023d7326a6
jsc284 <jsc284@scarletmail.rutgers.edu>
2021-11-04 00:54:09 -0400

    Edit 8: Laying Out the "Mine" Tableau
    Created new script "LayoutProto". Edited script "LayoutProto" according to pages 665-667.
    Edited script "ProspectorProto" according to pages 667-668.
    Attached script "LayoutProto" to object "_MainCamera" as script component.
    Set layoutXML of script component "ProspectorProto" of object "_MainCamera" as LayoutXML.

--- Assets/OtherGame/__Scripts/ProspectorProto.cs
+++ Assets/OtherGame/__Scripts/ProspectorProto.cs
@@ -10,8 +10,10 @@ public class ProspectorProto : MonoBehaviour
 
     [Header("Set in Inspector")]
     public TextAsset deckXML;
+    public TextAsset layoutXML;
     [Header("Set Dynamically")]
     public Deck deck;
+    public Layout layout;
 
     void Awake()
     {
@@ -24,12 +26,14 @@ public class ProspectorProto : MonoBehaviour
         deck.InitDeck(deckXML.text);
         Deck.Shuffle(ref deck.cards);
 
-        Card c;
+        /* Card c;
         for (int cNum=0; cNum<deck.cards.Count; cNum++)
         {
             c = deck.cards[cNum];
             c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
-        }
+        } */
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
     }
 
     // Update is called once per frame
